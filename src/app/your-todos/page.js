d8bbf721e1d1661dfb88c27f20913b3354062852
Manita Paudel <paudel04manita@gmail.com>
2024-06-19 12:28:39 +0545

    Refactor

--- src/app/your-todos/page.js
+++ src/app/your-todos/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button, Input, useDisclosure } from "@nextui-org/react";
+import { Button, useDisclosure } from "@nextui-org/react";
 import { useState } from "react";
 
 import { PlusIcon } from "../components/icons";
@@ -12,29 +12,22 @@ const YourTodos = () => {
 
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
-  const handleKeyDown = (e) => {
-    console.log(e);
-    if (e.key === "Enter") {
-      console.log("The task is", task);
-      setTask("");
-    }
-  };
-
   return (
     <>
-      <div className="w-1/2 mx-auto my-32">
-        <h1 className="font-medium">
-          Looks like you don&apos;t have any to-dos yet.
-        </h1>
-        <Button
-          className="w-full"
-          variant="ghost"
-          color="success"
-          startContent={<PlusIcon className={"w-4 h-4"} />}
-          onPress={onOpen}
-        >
-          Add Task
-        </Button>
+      <div className="w-1/3 mx-auto my-32">
+        <div className="flex items-center justify-between">
+          <h1 className="font-bold text-xl">
+            To-<span>Dos</span>
+          </h1>
+          <Button
+            variant="ghost"
+            color="success"
+            startContent={<PlusIcon className={"w-4 h-4"} />}
+            onPress={onOpen}
+          >
+            Add Task
+          </Button>
+        </div>
 
         <TaskList />
       </div>
