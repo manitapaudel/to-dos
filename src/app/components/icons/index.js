eed6bf7b1550ba62434dd211baef5fabe791d171
Manita Paudel <paudel04manita@gmail.com>
2024-06-19 11:14:48 +0545

    Add event handler

--- src/app/components/icons/index.js
+++ src/app/components/icons/index.js
@@ -1,3 +1,4 @@
+export { default as CloseIcon } from "./close-icon";
 export { default as EditIcon } from "./edit-icon";
 export { default as PlusIcon } from "./plus-icon";
 export { default as TrashIcon } from "./trash-icon";
--- src/app/your-todos/page.js
+++ src/app/your-todos/page.js
@@ -3,11 +3,23 @@
 import { Button, Input } from "@nextui-org/react";
 import { useState } from "react";
 
-import { PlusIcon } from "../components/icons";
+import { CloseIcon, PlusIcon } from "../components/icons";
 
 const YourTodos = () => {
   const [task, setTask] = useState("");
   const [showInput, setShowInput] = useState(false);
+
+  const handleClick = () => {
+    setShowInput((prev) => !prev);
+  };
+
+  const handleKeyDown = (e) => {
+    console.log(e);
+    if (e.key === "Enter") {
+      console.log("The task is", task);
+      setTask("");
+    }
+  };
 
   return (
     <div className="w-1/2 mx-auto my-32">
@@ -15,12 +27,15 @@ const YourTodos = () => {
         Looks like you don&apos;t have any to-dos yet.
       </h1>
       <Button
+        className="w-full"
         variant="ghost"
         color="success"
-        startContent={<PlusIcon className={"w-4 h-4"} />}
-        onClick={() => setShowInput(true)}
+        startContent={
+          showInput ? <CloseIcon /> : <PlusIcon className={"w-4 h-4"} />
+        }
+        onClick={handleClick}
       >
-        Add Task
+        {showInput ? "Close" : "Add Task"}
       </Button>
       {showInput ? (
         <Input
@@ -29,9 +44,10 @@ const YourTodos = () => {
           type="text"
           label="Task name"
           placeholder="Enter your task here"
-          className={`max-w-xs mt-5`}
+          className={` mt-5`}
           color="success"
           onChange={(e) => setTask(e.target.value)}
+          onKeyUp={handleKeyDown}
         />
       ) : (
         ""
