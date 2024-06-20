b27942d1bed45f5462e02aa9eb3d7426261cbf6f
Manita Paudel <paudel04manita@gmail.com>
2024-06-20 11:05:47 +0545

    Todo page responsiveness

--- src/app/components/todos/index.jsx
+++ src/app/components/todos/index.jsx
@@ -37,8 +37,8 @@ const ToDos = () => {
   };
 
   return (
-    <>
-      <div className="w-1/2 xl:w-1/3 mx-auto my-32 border border-2 border-green-400 px-4 py-10 rounded-md">
+    <div className="px-5">
+      <div className="sm:w-2/3 lg:w-1/2 mx-auto my-32 border border-2 border-green-400 px-4 py-10 rounded-md">
         <div className="flex items-center justify-between">
           <h1 className="font-semibold text-3xl">
             To-<span>Dos</span>
@@ -77,7 +77,7 @@ const ToDos = () => {
         isInvalid={isInvalid}
         errorMessage={errorMessage}
       />
-    </>
+    </div>
   );
 };
 
